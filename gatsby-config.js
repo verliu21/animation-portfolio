1c9df2a710b8e5c918be3dca549f46175c66c3fc
Veronica Liu <veronica.liu@utdallas.edu>
2018-10-20 07:53:09 -0500

    added pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Veronica Liu',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Veronica Liu</h1>
+    <p>University of Texas at Dallas 2020</p>
+    <p> UNDER CONSTRUCTION- more content soon!</p>
+    <Link to="/resume/">Resume</Link>
+    <Link to="/animation/">Animation</Link>
+    <Link to="/lighting/">Lighting </Link>
+    <Link to="/blog/">Blog</Link>
+    <Link to="/contact/">Contact</Link>
   </Layout>
 )
 
