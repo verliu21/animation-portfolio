50370af785de583cbc597fcfea8ef68d335f1dcb
Veronica Liu <veronica.liu@utdallas.edu>
2018-10-25 03:29:57 -0500

    contact too

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,16 @@ import Layout from '../components/layout'
 
 const ContactPage = () => (
   <Layout>
-    <h1>Contact info</h1>
-    <p>verliu21@gmail.com</p>
-    <p>contact box!</p>
+    <h1>Tell me cool things</h1>
+    <form enctype="text/plain" method="post" action="https://formspree.io/veronicaliu821@gmail.com">
+      <input type="text" name="firstname" placeholder="First Name"></input><br />
+      <input type="text" name="lastname" placeholder="Last Name"></input>
+
+      <input type="text" name="email" placeholder="Email"></input>
+
+      <textarea id="text" name="subject" placeholder="who R u"></textarea>
+      <input type="submit" value="Submit"></input>
+    </form>
   </Layout>
 )
 
