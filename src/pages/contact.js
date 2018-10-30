7658976875ef9ff0c3bc1ba73d9061d9a5dd79cd
Veronica Liu <veronica.liu@utdallas.edu>
2018-10-30 10:22:20 -0500

    updated lighting page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,10 +6,8 @@ const ContactPage = () => (
   <Layout>
     <h1>Tell me cool things</h1>
     <form method="POST" action="https://formspree.io/veronicaliu821@gmail.com">
-      <input type="text" name="firstname" placeholder="First Name"></input><br />
-      <input type="text" name="lastname" placeholder="Last Name"></input> <br/>
-
-      <input type="email" name="email" placeholder="Email"></input> <br/>
+      <input type="text" name="name" placeholder="Name"></input><br />
+      <input type="text" name="email" placeholder="Email"></input> <br/>
 
       <textarea id="text" name="subject" placeholder="hi there"></textarea>
       <input type="submit" value="Submit"></input>
--- src/pages/lighting.js
+++ src/pages/lighting.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import exteriorlight from "../images/exteriorlighting1.jpg"
 import interiorlight from "../images/interior-lighting4.png"
+import exteriorlight2 from "../images/exteriorlighting2.jpg"
 
 import Layout from '../components/layout'
 
@@ -8,8 +9,9 @@ const LightingPage = () => (
   <Layout>
     <h1>Lighting 1 </h1>
     <p>thanks for stopping by</p>
+    <img src={exteriorlight2} alt=" exterior lighting example" />
     <img src={exteriorlight} alt=" exterior lighting example" />
-    <img src={interiorlight} alt=" exterior lighting example" />
+    <img src={interiorlight} alt=" interior lighting example" />
     
   </Layout>
 )
